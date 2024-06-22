73622d49c9adcb8612e55881b9863f494de80504
Gabriel Silva <yzgabrieel02@gmail.com>
2024-06-22 01:29:19 -0300

    update `name` of functions

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-function NewImageOne() {
-    document.querySelector('.container').innerHTML = '<div class="container">   <div class="content"> <h1>A Sua Lâmpada Está Acesa!</h1>  <img src="./assets/images/On.png"> <button class="on" onclick="NewImageOne()">Acender</button> <button class="off" onclick="NewImageTwo()">Apagar</button>  </div> </div>  '
+function LampIsOn() {
+  document.querySelector(".container").innerHTML =
+    '<div class="container">   <div class="content"> <h1>A Sua Lâmpada Está Acesa!</h1>  <img src="./assets/images/On.png"> <button class="on" onclick="NewImageOne()">Acender</button> <button class="off" onclick="NewImageTwo()">Apagar</button>  </div> </div>  ';
 }
 
-function NewImageTwo() {
-    document.querySelector('.container').innerHTML = '<div class="container">   <div class="content"> <h1>A Sua Lâmpada Está Apagada!</h1>  <img src="./assets/images/Off.png"> <button class="on" onclick="NewImageOne()">Acender</button> <button class="off" onclick="NewImageTwo()">Apagar</button>  </div> </div>  '
+function LampIsOff() {
+  document.querySelector(".container").innerHTML =
+    '<div class="container">   <div class="content"> <h1>A Sua Lâmpada Está Apagada!</h1>  <img src="./assets/images/Off.png"> <button class="on" onclick="NewImageOne()">Acender</button> <button class="off" onclick="NewImageTwo()">Apagar</button>  </div> </div>  ';
 }
